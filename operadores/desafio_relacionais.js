5268ef9d62b549d30502a3ff4ccefd1cae079324
miguelbagual <miguel.aguilar01099462@sesisenaipr.org.br>
2026-03-06 11:39:18 -0300

    Operadores de atribuição e relacionais

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,19 @@
+let a = 10
+let b = 11
+
+// Imprimir no console 5 números verdadeiros
+
+console.log(a == a);
+console.log(a < b);
+console.log(a != b);
+console.log(b > a);
+console.log(b === b);
+
+console.log(" ");
+// Imprimir no console 5 números falsos
+
+console.log(a == b);
+console.log(a === b);
+console.log(a > b);
+console.log(b < a);
+console.log(a >= b);
---
+++ operadores/relacionais2.js
@@ -0,0 +1,15 @@
+let a = 1; //number
+let b = "1"; //string
+let c = "1"; //string
+
+// = atribuição
+// == somente valor
+// === valor e tipo
+
+console.log(a == b);
+console.log(a === b);
+console.log( b === c);
+
+console.log(a != b);
+console.log(a !== b);
+console.log(b !== c);
