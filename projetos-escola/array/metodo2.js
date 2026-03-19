a3a38a0c596040369a278baa2bace21fd7488357
miguelbagual <miguel.aguilar01099462@sesisenaipr.org.br>
2026-03-19 12:13:43 -0300

    Funcoes

---
+++ projetos-escola/array/metodo2.js
@@ -0,0 +1,10 @@
+const numeros = [1, 2, 3, 4, 8];
+
+//Desafio: remover o último elemento
+//2Minuto para realizar.
+
+numeros.pop( );
+console.log(numeros);
+
+numeros.splice(1, 2);
+console.log(numeros);
---
+++ projetos-escola/funcoes/sem_funcao.js
@@ -0,0 +1,17 @@
+//Sintaxe da função
+/*
+function nomeDaFuncao (parâmetros) {
+    //passos a serem executados
+}
+*/
+
+const produto1 = "Notebook";
+const preco1 = 3500;
+
+//Desafio: Imprimir produto e preço utilizando template string
+console.log(`Produto: ${produto1}. \nPreco: R$${preco1}.`);
+
+const produto2 = "Mouse";
+const preco2 = 150;
+
+console.log(`Produto: ${produto2}. \nPreco: R$${preco2}.`);
