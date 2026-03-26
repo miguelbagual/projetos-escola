d64619c61fe8f026810be04d3a2cd60a6066fcb1
miguelbagual <miguel.aguilar01099462@sesisenaipr.org.br>
2026-03-26 11:43:04 -0300

    Atividade2

---
+++ projetos-escola/atividades/ativ2.js
@@ -0,0 +1,5 @@
+let idadeCachorro = 6;
+let CachorroPHumano = idadeCachorro*7;
+
+console.log(`Se um cachorro tem ${idadeCachorro} anos, ele teria ${CachorroPHumano} anos em anos humanos.
+Isso acontece pois um ano de idade canina é equivalente a 7 anos humanos.`);
