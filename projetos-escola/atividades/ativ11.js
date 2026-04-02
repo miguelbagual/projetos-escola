929cc4b5f96ab8f475d695b9123040786c32ebae
miguelbagual <miguel.aguilar01099462@sesisenaipr.org.br>
2026-04-02 12:01:26 -0300

    Atividade 11

---
+++ projetos-escola/atividades/ativ11.js
@@ -0,0 +1,9 @@
+const nomes = ["Miguel", "Pedro Potricao", "Emanuel", "Felipao", "Rafael", "Abdal Papaulin", "Alex o Leao", "Pedro Lima", "Pedrao", "Gustavo Uncini Seleção Brasileira"];
+let nome = "Miguel"
+
+if(nomes.includes(nome)==true){
+    console.log("Usuário encontrado no sistema.");
+}
+else{
+    console.log("Usuário não localizado.")
+}
