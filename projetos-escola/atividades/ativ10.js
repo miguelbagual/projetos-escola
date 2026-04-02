26023079649f4749904a10dde7b5440669db54fd
miguelbagual <miguel.aguilar01099462@sesisenaipr.org.br>
2026-04-02 11:43:10 -0300

    Atividade 10

---
+++ projetos-escola/atividades/ativ10.js
@@ -0,0 +1,12 @@
+const hardware = ["Memória RAM","Placa de Vídeo", "Processador", "Placa Mãe", "SSD"];
+
+console.log(hardware);
+
+hardware.push("Fonte");
+
+console.log(hardware);
+
+hardware.shift(1);
+
+console.log(hardware);
+
