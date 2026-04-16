41279fb000a3793a1f94d8c6ce88dddc70cb1257
miguelbagual <miguel.aguilar01099462@sesisenaipr.org.br>
2026-04-16 10:35:17 -0300

    Desafio Final

--- projetos-escola/atividades/atividade12.js
+++ projetos-escola/atividades/atividade12.js
@@ -1,6 +1,6 @@
-const estoque = ["Memória RAM","Placa de Vídeo", "Processador", "Placa Mãe", "SSD"]; //Criação do estoque com produtos
+const estoque = ["Memória RAM","Placa de Vídeo", "Processador", "Placa Mãe", "SSD"];       //Criação do estoque com produtos
 
-estoque.push("Fonte");//Adicionando mais um produto ao estoque
+estoque.push("Fonte");      //Adicionando mais um produto ao estoque
 
 //Declaração das variaveis
 let produtosComprados = 3;
@@ -13,36 +13,37 @@ let vendaPossivel;
 
 //Função para o desconto
 function processarVenda(valorTotal, possuiCupom){
-    let valorFinal = valorTotal>500||possuiCupom==true ? valorTotal=valorTotal*0.85 : valorTotal; //Se o valor total for maior que 500, ou se possuir cupom for true, aplicar 15% de desconto.
+    let valorFinal = valorTotal>500||possuiCupom==true ? valorTotal=valorTotal*0.85 : valorTotal;       //Se o valor total for maior que 500, ou se possuir cupom for true, aplicar 15% de desconto.
     
     return valorFinal;
 }
-console.log(`Valor da compra: R$${processarVenda(valorTotal, possuiCupom)}`);//Exibindo o return da função.
+console.log(`Valor da compra: R$${processarVenda(valorTotal, possuiCupom)}`);      //Exibindo o return da função.
 
 //If para verificar a idade, se maior que 16, a venda é autorizada.
 if(clienteIdade>=16){
     console.log(`Venda autorizada para ${clienteNome}`);
-    vendaPossivel = true; //Variavel venda possivel, para caso cliente seja menor que 16, nao permitir a venda.
+    vendaPossivel = true;       //Variavel venda possivel, para caso cliente seja menor que 16, nao permitir a venda.
 }
 else{
     console.log(`Venda bloqueada: Idade insuficiente`);
     vendaPossivel = false;
 }
 
-console.log("\n")
+console.log("\n")      //Espaço em branco para organizar
 
 //Se a venda for possivel (cliente maior que 16 anos)
 if(vendaPossivel==true){
-    for(let i = 0; i<produtosComprados; i++){ //Loop for com variavel produtos comprados, para facilitar o uso do programa.
-        console.log(`Despachando item: ${estoque[i]}... OK!`);//estoque[i], o console.log vai mostrar o produto correspondente a iteração do loop for.
+    for(let i = 0; i<produtosComprados; i++){       //Loop for com variavel produtos comprados, para facilitar o uso do programa.
+        console.log(`Despachando item: ${estoque[i]}... OK!`);      //estoque[i], o console.log vai mostrar o produto correspondente a iteração do loop for.
     }
 
-    for(let i = 0; i<produtosComprados; i++){//Outro for, para nao cortar os produtos antes do console.log
-        estoque.shift()//Remover o primeiro produto.
+    for(let i = 0; i<produtosComprados; i++){      //Outro for, para nao cortar os produtos antes do console.log
+        estoque.shift()      //Remover o primeiro produto.
     }
 
-quantidadeItens = estoque.length//Definir quantidade de itens como a quantidade de itens do estoque.
-    console.log("\n")
+quantidadeItens = estoque.length      //Definir variavel quantidade de itens como a quantidade de itens do estoque.
+
+console.log("\n")      //Espaço em branco para organizar
 
 //Ultimo console.log para fazer um relatório da compra.
     console.log(`Relatório da Loja:
